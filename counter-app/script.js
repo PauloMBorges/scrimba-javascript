88225fb8baa562b405518fa0b5459a2f8551cd47
PauloMBorges <paulodemeloborges@gmail.com>
2024-07-08 19:41:40 -0300

    Change onclick to addEventListener

--- counter-app/script.js
+++ counter-app/script.js
@@ -2,6 +2,12 @@ let countEl = document.getElementById('count-el')
 let count = 0
 let saveEl = document.getElementById('save-el')
 let history = ""
+let incrementBtn = document.getElementById('increment-btn')
+incrementBtn.addEventListener('click', increment)
+let decrementBtn = document.getElementById('decrement-btn')
+decrementBtn.addEventListener('click', decrement)
+let saveBtn = document.getElementById('save-btn')
+saveBtn.addEventListener('click', save)
 
 function increment() {
     count += 1
